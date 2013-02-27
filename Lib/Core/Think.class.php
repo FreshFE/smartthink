ba2478b6595699161332d20409032045519f5360
MinoWu <minowu@foxmail.com>
2013-02-28 00:15:15 +0800

    添加加载额外扩展配置文件的能力。

--- Lib/Core/Think.class.php
+++ Lib/Core/Think.class.php
@@ -54,9 +54,16 @@ class Think {
         }else{
             $mode   =  array();
         }
+
         // 加载核心惯例配置文件
         C(include THINK_PATH.'Conf/convention.php');
-        if(isset($mode['config'])) {// 加载模式配置文件
+
+        // 加载扩展配置文件
+        if(is_file(EXTEND_PATH . 'Conf/convention.php'))
+            C(include EXTEND_PATH . 'Conf/convention.php');
+
+        // 加载模式配置文件
+        if(isset($mode['config'])) {
             C( is_array($mode['config'])?$mode['config']:include $mode['config'] );
         }
 
