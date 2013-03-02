4b3b70b833d6a3c0a117809ae412d89d6e1fc68c
MinoWu <minowu@foxmail.com>
2013-03-02 18:08:23 +0800

    删除alias里相关的内容。

--- Conf/alias.php
+++ Conf/alias.php
@@ -15,8 +15,9 @@ return array(
     'Model'         => CORE_PATH.'Core/Model.class.php',
     'Db'            => CORE_PATH.'Core/Db.class.php',
     'Log'          	=> CORE_PATH.'Core/Log.class.php',
-    'ThinkTemplate' => CORE_PATH.'Template/ThinkTemplate.class.php',
-    'TagLib'        => CORE_PATH.'Template/TagLib.class.php',
+    // TODO: 删除ThinkTemplate相关内容
+    // 'ThinkTemplate' => CORE_PATH.'Template/ThinkTemplate.class.php',
+    // 'TagLib'        => CORE_PATH.'Template/TagLib.class.php',
     'Cache'         => CORE_PATH.'Core/Cache.class.php',
     'Widget'        => CORE_PATH.'Core/Widget.class.php',
     'TagLibCx'      => CORE_PATH.'Driver/TagLib/TagLibCx.class.php',
