1aa0981fdd12bead163b55cbc1cc76a2b66348bb
MinoWu <minowu@foxmail.com>
2013-03-20 22:53:09 +0800

    调整注释。

--- Lib/Core/Redirect.class.php
+++ Lib/Core/Redirect.class.php
@@ -1,28 +1,61 @@
 <?php
+/**
+ * Core/Redirect.class.php
+ * Smart ThinkPHP
+ * Copyright (c) 2004-2013 Methink
+ *
+ * @copyright     Copyright (c) Methink
+ * @link          https://github.com/minowu/thinkphp
+ * @package       Core.Redirect
+ * @since         Smart ThinkPHP 2.0.0
+ * @license       Apache License (http://www.apache.org/licenses/LICENSE-2.0)
+ */
 
+/**
+ * 帮助页面跳转的类
+ */
 class Redirect {
 
 	/**
+	 * 如果用户操作成功跳转
+	 *
+	 * @param string $message 提示信息
+	 * @param string $url 跳转地址
+	 *
+	 * @return void
 	 */
-	static public function succees(string $message, string $jumpUrl) {
+	static public function succees(string $message, string $url) {
 
-		Redirect::to($message, true, $jumpUrl);
+		Redirect::to($message, $url, true);
 	}
 
 	/**
+	 * 如果用户操作失败跳转
+	 *
+	 * @param string $message 提示信息
+	 * @param string $url 跳转地址
+	 *
+	 * @return void
 	 */
-	static public function error(string $message, string $jumpUrl) {
+	static public function error(string $message, string $url) {
 
-		Redirect::to($message, false, $jumpUrl);
+		Redirect::to($message, $url, false);
 	}
 
 	/**
+	 * 页面跳转
+	 *
+	 * @param string $message 提示信息
+	 * @param bealoon $status 操作成功或失败
+	 * @param string $url 跳转地址
+	 *
+	 * @return void
 	 */
-	static public function to(string $message, bealoon $status, string $jumpUrl) {
+	static public function to(string $message, string $url, bealoon $status) {
 
 	    // 设置路径
-	    if(!$jumpUrl) {
-	    	$jumpUrl = empty($_SERVER["HTTP_REFERER"]) ? __APP__ : $_SERVER["HTTP_REFERER"];
+	    if(!$url) {
+	    	$url = empty($_SERVER["HTTP_REFERER"]) ? __APP__ : $_SERVER["HTTP_REFERER"];
 	    }
 
 	    // 设置提示信息session
@@ -33,20 +66,22 @@ class Redirect {
 	    }
 
 	    // 重定向
-	    Redirect::send($jumpUrl);
+	    Redirect::send($url);
 	}
 
 	/**
+	 * 执行页面跳转
+	 *
+	 * @param string $url 跳转地址
+	 *
+	 * @return void
 	 */
-	static private function send($url) {
+	static private function send(string $url) {
 
-		$url = str_replace(array("\n", "\r"), '', $url);
-
-	    if (!headers_sent()) {
+	    if(!headers_sent()) {
 	        header('Location: ' . $url);
-	        exit();
-	    } else {
-	    	exit('跳转失败');
 	    }
+
+	    exit('跳转失败');
 	}
 }
