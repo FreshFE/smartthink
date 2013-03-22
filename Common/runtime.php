2d5effeba2dc45ed9e3f471de84858348c77eda8
MinoWu <minowu@foxmail.com>
2013-03-22 15:58:02 +0800

    调整注释，临时关闭build_runtime_cache函数。

--- Common/runtime.php
+++ Common/runtime.php
@@ -1,40 +1,36 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2006-2012 http://thinkphp.cn All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: liu21st <liu21st@gmail.com>
-// +----------------------------------------------------------------------
-
 /**
  * ThinkPHP 运行时文件 编译后不再加载
  * @category   Think
  * @package  Common
  * @author   liu21st <liu21st@gmail.com>
  */
+
+// 文件访问需求控制
 defined('THINK_PATH') or exit();
 if(version_compare(PHP_VERSION,'5.2.0','<'))  die('require PHP > 5.2.0 !');
 
-//  版本信息
-define('THINK_VERSION', '3.1.2');
+// 框架版本
+define('THINK_VERSION', '4.0.0 Preview');
 
-//   系统信息
+// 系统信息
 if(version_compare(PHP_VERSION,'5.4.0','<')) {
-    ini_set('magic_quotes_runtime',0);
-    define('MAGIC_QUOTES_GPC',get_magic_quotes_gpc()?True:False);
-}else{
+    ini_set('magic_quotes_runtime', 0);
+    define('MAGIC_QUOTES_GPC', get_magic_quotes_gpc() ? true : false);
+}
+else {
     define('MAGIC_QUOTES_GPC',false);
 }
-define('IS_CGI',substr(PHP_SAPI, 0,3)=='cgi' ? 1 : 0 );
-define('IS_WIN',strstr(PHP_OS, 'WIN') ? 1 : 0 );
-define('IS_CLI',PHP_SAPI=='cli'? 1   :   0);
+
+// 当前系统和操作方式
+define('IS_CGI', substr(PHP_SAPI, 0, 3) == 'cgi' ? 1 : 0 );
+define('IS_WIN', strstr(PHP_OS, 'WIN') ? 1 : 0);
+define('IS_CLI', PHP_SAPI == 'cli' ? 1 : 0);
 
 // 项目名称
 defined('APP_NAME') or define('APP_NAME', basename(dirname($_SERVER['SCRIPT_FILENAME'])));
 
+// 在非命令行执行中定义
 if(!IS_CLI) {
     // 当前文件名
     if(!defined('_PHP_FILE_')) {
@@ -90,8 +86,11 @@ set_include_path(get_include_path() . PATH_SEPARATOR . VENDOR_PATH);
 // 加载运行时所需要的文件 并负责自动目录生成
 function load_runtime_file() {
 
+    // TODO: 重新编辑该文件，对象化
     // 加载系统基础函数库
     require THINK_PATH.'Common/common.php';
+
+    // TODO: 分析重构
     // 读取核心文件列表
     $list = array(
         CORE_PATH.'Core/Think.class.php',
@@ -102,48 +101,68 @@ function load_runtime_file() {
     foreach ($list as $key=>$file){
         if(is_file($file))  require_cache($file);
     }
+
+    // TODO: 分析删除该代码
     // 加载系统类库别名定义
     alias_import(include THINK_PATH.'Conf/alias.php');
 
-    // 检查项目目录结构 如果不存在则自动创建
+    // 创建项目目录结构
     if(!is_dir(LIB_PATH)) {
-        // 创建项目目录结构
-        build_app_dir();
-    }elseif(!is_dir(CACHE_PATH)){
-        // 检查缓存目录
+        throw_exception('不存在项目目录结构');
+    }
+
+    // 检查缓存目录
+    elseif(!is_dir(CACHE_PATH)) {
         check_runtime();
-    }elseif(APP_DEBUG){
-        // 调试模式切换删除编译缓存
-        if(is_file(RUNTIME_FILE))   unlink(RUNTIME_FILE);
+    }
+
+    // 调试模式切换删除编译缓存
+    elseif(APP_DEBUG){
+        if(is_file(RUNTIME_FILE)) {
+            unlink(RUNTIME_FILE);
+        }
     }
 }
 
 // 检查缓存目录(Runtime) 如果不存在则自动创建
 function check_runtime() {
+
+    // 如果不存在Runtime则创建
     if(!is_dir(RUNTIME_PATH)) {
         mkdir(RUNTIME_PATH);
-    }elseif(!is_writeable(RUNTIME_PATH)) {
+    }
+
+    // 如果Runtime不可写返回
+    elseif(!is_writeable(RUNTIME_PATH)) {
         header('Content-Type:text/html; charset=utf-8');
         exit('目录 [ '.RUNTIME_PATH.' ] 不可写！');
     }
-    mkdir(CACHE_PATH);  // 模板缓存目录
+
+    // 创建模板缓存目录
+    if(!is_dir(CACHE_PATH)) mkdir(CACHE_PATH);  // 模板缓存目录
     if(!is_dir(LOG_PATH))   mkdir(LOG_PATH);    // 日志目录
     if(!is_dir(TEMP_PATH))  mkdir(TEMP_PATH);   // 数据缓存目录
     if(!is_dir(DATA_PATH))  mkdir(DATA_PATH);   // 数据文件目录
+
     return true;
 }
 
 // 创建编译缓存
 function build_runtime_cache($append='') {
+
+    exit('临时关闭生成编译文件');
+
     // 生成编译文件
     $defs           = get_defined_constants(TRUE);
-    $content        =  '$GLOBALS[\'_beginTime\'] = microtime(TRUE);';
+    $content        = '$GLOBALS[\'_beginTime\'] = microtime(TRUE);';
+
     if(defined('RUNTIME_DEF_FILE')) { // 编译后的常量文件外部引入
         file_put_contents(RUNTIME_DEF_FILE,'<?php '.array_define($defs['user']));
         $content   .=  'require \''.RUNTIME_DEF_FILE.'\';';
     }else{
         $content   .= array_define($defs['user']);
     }
+
     $content       .= 'set_include_path(get_include_path() . PATH_SEPARATOR . VENDOR_PATH);';
     // 读取核心编译文件列表
     $list = array(
@@ -165,6 +184,7 @@ function build_runtime_cache($append='') {
     file_put_contents(RUNTIME_FILE,strip_whitespace('<?php '.str_replace("defined('THINK_PATH') or exit();",' ',$content)));
 }
 
+// TODO: 与build_runtime_cache同时编辑
 // 编译系统行为扩展类库
 function build_tags_cache() {
     $tags = C('extends');
@@ -177,8 +197,9 @@ function build_tags_cache() {
     return $content;
 }
 
+// TODO: 删除该代码
 // 创建项目目录结构
-function build_app_dir() {
+/*function build_app_dir() {
     // 没有创建项目目录的话自动创建
     if(!is_dir(APP_PATH)) mkdir(APP_PATH,0755,true);
     if(is_writeable(APP_PATH)) {
@@ -214,33 +235,37 @@ function build_app_dir() {
         header('Content-Type:text/html; charset=utf-8');
         exit('项目目录不可写，目录无法自动生成！<BR>请使用项目生成器或者手动生成项目目录~');
     }
-}
+}*/
 
-// 创建测试Action
-function build_first_action() {
-    $content = file_get_contents(THINK_PATH.'Tpl/default_index.tpl');
-    file_put_contents(LIB_PATH.'Action/IndexAction.class.php',$content);
-}
+// TODO: 删除代码
+// // 创建测试Action
+// function build_first_action() {
+//     $content = file_get_contents(THINK_PATH.'Tpl/default_index.tpl');
+//     file_put_contents(LIB_PATH.'Action/IndexAction.class.php',$content);
+// }
 
+// TODO: 转移代码至对象内
 // 生成目录安全文件
-function build_dir_secure($dirs='') {
-    // 目录安全写入
-    if(defined('BUILD_DIR_SECURE') && BUILD_DIR_SECURE) {
-        defined('DIR_SECURE_FILENAME')  or define('DIR_SECURE_FILENAME',    'index.html');
-        defined('DIR_SECURE_CONTENT')   or define('DIR_SECURE_CONTENT',     ' ');
-        // 自动写入目录安全文件
-        $content = DIR_SECURE_CONTENT;
-        $files = explode(',', DIR_SECURE_FILENAME);
-        foreach ($files as $filename){
-            foreach ($dirs as $dir)
-                file_put_contents($dir.$filename,$content);
-        }
-    }
-}
+// function build_dir_secure($dirs='') {
+//     // 目录安全写入
+//     if(defined('BUILD_DIR_SECURE') && BUILD_DIR_SECURE) {
+//         defined('DIR_SECURE_FILENAME')  or define('DIR_SECURE_FILENAME',    'index.html');
+//         defined('DIR_SECURE_CONTENT')   or define('DIR_SECURE_CONTENT',     ' ');
+//         // 自动写入目录安全文件
+//         $content = DIR_SECURE_CONTENT;
+//         $files = explode(',', DIR_SECURE_FILENAME);
+//         foreach ($files as $filename){
+//             foreach ($dirs as $dir)
+//                 file_put_contents($dir.$filename,$content);
+//         }
+//     }
+// }
 
 // 加载运行时所需文件
 load_runtime_file();
+
 // 记录加载文件时间
 G('loadTime');
+
 // 执行入口
 Think::Start();
