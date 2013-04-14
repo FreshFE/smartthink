cada030785f4f72807805d39eeeee9ea30b35711
MinoWu <minowu@foxmail.com>
2013-04-15 01:29:09 +0800

    调整Config.php的注释。

--- Lib/Config.php
+++ Lib/Config.php
@@ -1,21 +1,35 @@
-<?php
-namespace Think;
+<?php namespace Think;
+/**
+ * SmartThink PHP
+ * Copyright (c) 2004-2013 Methink
+ * Thanks for ThinkPHP & GEM-MIS
+ * @copyright     Copyright (c) Methink
+ * @link          http://smartthink.org
+ * @package       Think.Config
+ * @since         SmartThink 1.0.0 & ThinkPHP 1.0.0
+ * @license       Apache License (http://www.apache.org/licenses/LICENSE-2.0)
+ */
 
-class Config {
-
+/**
+ * 框架常被用到的类
+ * 用于获取和设置系统和项目配置
+ * 由ThinkPHP的C函数演变而来
+ */
+class Config
+{
 	/**
 	 * 配置静态存储
 	 *
-	 * var array
+	 * @var array
 	 */
 	private static $storage = array();
 
 	/**
 	 * 设置一个配置
 	 *
+	 * @stability: 3, 接口已经稳定，但是现实方法会改变，逐步删除二维数组设置和获取支持
 	 * @param string $name 配置名称
 	 * @param mix $value 配置值
-	 *
 	 * @return mix
 	 */
 	public static function set($name, $value = null) {
@@ -44,8 +58,8 @@ class Config {
 	/**
 	 * 得到一个配置
 	 *
+	 * @stability: 3, 接口已经稳定，但是现实方法会改变，逐步删除二维数组设置和获取支持
 	 * @param string $name 配置名称
-	 *
 	 * @return mix
 	 */
 	public static function get($name) {
@@ -66,8 +80,8 @@ class Config {
 	/**
 	 * 根据数组批量设置
 	 *
+	 * @stability: 4
 	 * @param array $array 配置数组
-	 *
 	 * @return array
 	 */
 	public static function setAll($array) {
@@ -78,6 +92,7 @@ class Config {
 	/**
 	 * 获得所有的配置列表
 	 *
+	 * @stability: 4
 	 * @return array
 	 */
 	public static function getAll() {
