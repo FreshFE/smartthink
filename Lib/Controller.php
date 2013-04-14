4f94f8987831bb9497c9d941ad0ffa8209d87f7a
MinoWu <minowu@foxmail.com>
2013-04-15 01:42:03 +0800

    调整Controller的注释。

--- Lib/Controller.php
+++ Lib/Controller.php
@@ -1,18 +1,65 @@
-<?php
-namespace Think;
+<?php namespace Think;
+/**
+ * SmartThink PHP
+ * Copyright (c) 2004-2013 Methink
+ * Thanks for ThinkPHP & GEM-MIS
+ * @copyright     Copyright (c) Methink
+ * @link          http://smartthink.org
+ * @package       Think.Behavior
+ * @since         ThinkPHP 3.0.0
+ * @license       Apache License (http://www.apache.org/licenses/LICENSE-2.0)
+ */
 
+use Think\Config as Config;
+use Think\Log as Log;
+use Think\View as View;
+use Think\Response as Response;
+use Think\File as File;
+use Think\Debug as Debug;
 
+/**
+ * 控制器抽象类，供项目开发的控制器类继承
+ * 内置了关于模板的视图操作调用方法，assign, display & fetch
+ * 添加了json输出的快捷方法
+ * TODO: 计划逐步删除__call方法里的内容
+ */
 abstract class Controller {
 
+    /**
+     * 实例化后的视图类容器
+     *
+     * @var object
+     */
     protected $view;
 
+    /**
+     * 存放备输出数组的容器
+     * 由$this->assign()方法添加
+     * 被display, fetch & json使用
+     *
+     * @var array
+     */
     protected $vars = array();
 
+    /**
+     * 构造函数
+     * 实例化视图类
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->view = new View();
     }
 
+    /**
+     * 将要输出的内容存入$this->vars
+     *
+     * @stability: 4
+     * @param string|array $name
+     * @param mixed $value
+     * @return void
+     */
     protected function assign($name, $value)
     {
         if(is_array($name)) {
@@ -23,16 +70,40 @@ abstract class Controller {
         }
     }
 
+    /**
+     * 调用view的display接口
+     * 将$this->vars输出到模板
+     *
+     * @stability: 4
+     * @param string $name
+     * @return void
+     */
     protected function display($name)
     {
         $this->view->display($name, $this->vars);
     }
 
+    /**
+     * 调用view的fetch接口
+     * 将$this->vars输出到模板，并返回最后生成的字符串
+     *
+     * @stability: 4
+     * @param string $name
+     * @return string
+     */
     protected function fetch($name)
     {
         return $this->view->fetch($name, $this->vars);
     }
 
+    /**
+     * 将$this->vars解析成json输出，并结束程序
+     *
+     * @stability: 3
+     * @param string|array|null $name
+     * @param mixed|null $value
+     * @return void
+     */
     protected function json($name = null, $value = null)
     {
         if(!is_null($name))
@@ -120,8 +191,9 @@ abstract class Controller {
         }
     }
 
-   /**
+    /**
      * 析构方法
+     * 根据配置来调整是否添加Log记录
      *
      * @return void
      */
