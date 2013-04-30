19fdeaea624650dc81f5a68e11f04d47e6bdc759
Mino Wu <minowu@foxmail.com>
2013-04-30 13:07:52 +0800

    添加_empty方法，如果不存在指定的方法名，则根据请求类型合成。

--- Lib/Controller.php
+++ Lib/Controller.php
@@ -43,6 +43,13 @@ abstract class Controller {
     protected $vars = array();
 
     /**
+     * 允许检测的方法
+     *
+     * @var array
+     */
+    protected $_method = array('get', 'post', 'put', 'delete', 'head');
+
+    /**
      * 构造函数
      * 实例化视图类
      *
@@ -199,6 +206,51 @@ abstract class Controller {
                 $data       =	 isset($args[2])?$args[2]:NULL;
             }
             return $data;
+        }
+    }
+
+    /**
+     * 空方法执行请求方法属性的检查
+     * 查找相对应的get, post, put, delete等方法并执行
+     *
+     * @return void
+     */
+    public function _empty()
+    {
+        try
+        {
+            $method = $this->_method;
+
+            // 当前http请求方法
+            $current = strtolower($_SERVER['REQUEST_METHOD']);
+
+            if(in_array($current, $method))
+            {
+                $class = $current . '_' . ACTION_NAME;
+
+                // 是否存在this/get_login
+                if(method_exists($this, $class))
+                {
+                    $this->$class();
+                }
+                // 是否存在this/get
+                else if(method_exists($this, $current))
+                {
+                    $this->$current();
+                }
+                // 不存在输出
+                else {
+                    throw new Exception("不存在指定的控制器方法");
+                }
+            }
+            // 方法错误输出
+            else {
+                throw new Exception("不存在'$method'方法");
+            }
+
+        }
+        catch (Exception $e) {
+            Debug::output($e);
         }
     }
 
