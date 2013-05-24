8831e496fd0017736b3f2fc0ec7b984f76c30862
MinoWu <minowu@foxmail.com>
2013-05-24 13:26:58 +0800

    删除原CheckAuth的行为类，改为全新的CheckAuth行为。

--- Lib/Behaviors/CheckAuth.php
+++ Lib/Behaviors/CheckAuth.php
@@ -1,157 +1,78 @@
-<?php
-namespace Think\Behaviors;
-/**
- * Behavior/CheckAuthBehavior.class.php
- * Smart ThinkPHP
- * Copyright (c) 2004-2013 Methink
- *
- * @copyright     Copyright (c) Methink
- * @link          https://github.com/minowu/extend
- * @package       Behavior/CheckAuthBehavior
- * @since         Smart ThinkPHP 2.0.0
- * @license       Apache License (http://www.apache.org/licenses/LICENSE-2.0)
- */
+<?php namespace Think\Behaviors;
 
-use Think\Behavior as Behavior;
-use Think\Config as Config;
-use Think\Import as Import;
-use Think\Auth as Auth;
+use Think\Behavior;
+use Think\Config;
+use Think\Session;
+use Think\Cookie;
+use Think\Response;
+use Think\Exception;
+use Think\Auths\Authentication;
+use Think\Auths\Authorization;
 
-/**
- * CheckAuthBehavior Class
- * 根据载入不同的类包来实例化Auth类并执行访问权检查
- * 执行后将Auth类返回至Config内
- */
-class CheckAuth extends Behavior {
+class CheckAuth extends Behavior
+{
+	public $roleRules;
+
+	public $modelName = 'User';
 
 	/**
-	 * 基础配置
-	 * 在Behavior构造函数中，设置内容会和Config进行合并
-	 * 如果Config中存在该值，则不合并，不存在则将下列默认值加入Config内
+	 * 行为入口方法
 	 *
-	 * var array
-	 */
-	protected $options = array(
-
-		/**
-		 * 是否开启权限认证
-		 * true为开启，检查访问权
-		 * false为关闭，不检查访问权
-		 *
-		 * var bealoon
-		 */
-		'AUTH_ON' => false,
-
-		/**
-		 * 权限认证的规则
-		 * 必须在Config中定义
-		 *
-		 * var array
-		 */
-		'AUTH_RULES' => array(),
-
-		/**
-		 * 权限认证的角色
-		 * 必须在Config中定义
-		 *
-		 * var array
-		 */
-		'AUTH_ROLES' => array(),
-
-		/**
-		 * 权限认证的查询模型
-		 * 未使用功能
-		 *
-		 * var string
-		 */
-		'AUTH_USER_ROLE_MODEL' => '',
-
-		/**
-		 * 类包的加载名称
-		 *
-		 * var string
-		 */
-		'AUTH_PACKAGE_NAME' => 'Library/Auth/RbacAuth',
-
-		/**
-		 * 类包的加载路径
-		 *
-		 * var string
-		 */
-		'AUTH_PACKAGE_PATH' => CORE_PATH,
-
-		/**
-		 * Auth类包的名称
-		 *
-		 * var string
-		 */
-		'AUTH_CLASS' => 'Think\\Library\\RbacAuth',
-
-		/**
-		 * 已经登录的跳转地址
-		 *
-		 * var string
-		 */
-		'AUTH_LOGINED_URL' => 'Index/index',
-
-		/**
-		 * 未登录的跳转地址
-		 *
-		 * var string
-		 */
-		'AUTH_UNLOGIN_URL' => 'Account/login'
-	);
-
-	/**
-	 * 实现Behavior的抽象方法
-	 * 运行CheckAuthBehavior类
-	 *
-	 * @param mix &$return
-	 *
+	 * @param mixed &$params
 	 * @return void
 	 */
-	public function run(&$return) {
-		
-		if(C('AUTH_ON')) {
+	public function run(&$params)
+	{
+		// 设置用户角色的规则
+		if(is_null($roleRules)) {
+			$this->roleRules = Config::get('AUTH_RULES');
+		}
 
-			// 开始check
-			$auth = $this->check();
+		// 认证
+		$authentication = $this->checkAuthentication($this->modelName);
 
-			// 将对象输出到全局
-			Auth::init($auth);
+		// 授权
+		$authorization = $this->checkAuthorization($authentication->getUserRole(), $this->roleRules);
+
+		// 授权未通过
+		if(!$authorization) {
+			if($authentication->getUserRole() == 'ROLE_ANONYMOUS') {
+				$this->redirectTo401();
+			}
+			else {
+				$this->redirectTo403();
+			}
 		}
 	}
 
+	public function redirectTo401()
+	{
+		Response::send_http_status(401);
+		exit();
+	}
+
+	public function redirectTo403()
+	{
+		Response::send_http_status(403);
+		exit();
+	}
+
+	protected function checkAuthentication($modelName)
+	{
+		$driver = new Authentication($modelName);
+
+		return $driver->check();
+	}
+
 	/**
-	 * 检查Auth功能
-	 * 可根据配置载入不同的类包和执行不同的类
-	 * 被执行类需要实现check, session和error三个方法
-	 *
-	 * @return object 实例化的auth对象
+	 * 检查授权
 	 */
-	private function check() {
+	protected function checkAuthorization($userRole, $roleRules)
+	{
+		// 实例化
+		$driver = new Authorization($userRole, $roleRules);
 
-		// 依赖载入
-		Import::load(CORE_PATH . 'Library/Auth/RbacAuth' . EXT);
-
-		// 类实例化
-		$class = C('AUTH_CLASS');
-		$auth = new $class(C('AUTH_KEY'), C('AUTH_RULES'), C('AUTH_ROLES'));
-
-		// 检查访问权
-		if(!$auth->check()) {
-
-			// 已经登录
-			if($auth->session()) {
-				$auth->error('您已经登录', U(C('AUTH_LOGINED_URL')));
-			}
-
-			// 未登录
-			else {
-				$auth->error('您未登录，请先登录', U(C('AUTH_UNLOGIN_URL')));
-			}
-		}
-
-		return $auth;
+		// 返回检查结果
+		return $driver->check();
 	}
 }
